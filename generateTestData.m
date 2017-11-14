2880b1ab6143fff33bfb8a6033eb76812ea2d46e
sdlpkxd <lpkhappy@yeah.net>
2017-11-14 19:42:59 +0800

    Update generateTestData.m

--- generateTestData.m
+++ generateTestData.m
@@ -1,39 +1,24 @@
 clear;close all;
 %% settings
-folder = 'D:\caffe_DnCNN\testData\BSD68';
-savepath = 'D:\caffe_DnCNN\testData\test.h5';
+folder = 'BSD68';
+savepath = 'test.h5';
 size_input = 40;
 size_label = 40;
 stride = 20;
 
-% folderTest  = 'C:\Users\sm\Desktop\CNNSR\FSRCNN\Train\VDSR';
-% ext         =  {'*.jpg','*.png','*.bmp'};
-% filePaths   =  [];
-% for i = 1 : length(ext)
-%     filePaths = cat(1,filePaths, dir(fullfile(folderTest,ext{i})));
-% end
-%% initialization
 data = zeros(size_input, size_input, 1, 1);
 label = zeros(size_label, size_label, 1, 1);
 padding = abs(size_input - size_label)/2;
 count = 0;
 noiseSigma = 8;
-%% generate data
+
 filepaths = dir(fullfile(folder,'*.png'));
 
-% for scale = 2:4
     for i = 1 : length(filepaths)
-%         fprintf('i:%d\n',i);
-
         image = imread(fullfile(folder,filepaths(i).name));
         image = im2double(image);
         [hei,wid] = size(image);
-%         image1 = im2double(image(:, :, 1));
-
         im_input = image;
-%         im_label = modcrop(image, scale);
-%         
-%         im_input = imresize(imresize(im_label,1/scale,'bicubic'),[hei,wid],'bicubic');
 
         for x = 1 : stride : hei-size_input+1
             for y = 1 :stride : wid-size_input+1
@@ -41,7 +26,6 @@ filepaths = dir(fullfile(folder,'*.png'));
                 randn('seed',0);
                 noise = noiseSigma/255*randn(size(subim_input));
                 subim_input = subim_input + noise;
-%                 subim_label = im_label(x+padding : x+padding+size_label-1, y+padding : y+padding+size_label-1);
                 subim_label = noise;
 
                 count=count+1;
@@ -49,8 +33,7 @@ filepaths = dir(fullfile(folder,'*.png'));
                 label(:, :, 1, count) = subim_label;
             end
         end
-    end
-% end
+    end    
 
 order = randperm(count);
 data = data(:, :, 1, order);
